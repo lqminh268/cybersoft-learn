b8d16d1bccfd18f46d87c9ee6fce253dcd0c1746
Minh268 <lq.minh268@gmail.com>
2022-01-04 11:52:59 +0700

    create header for homepage

--- app/modules/HomePage/index.jsx
+++ app/modules/HomePage/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Header from '../../components/Header'
+import '../../assets/css/fonts.css';
+import '../../assets/css/style.css';
 
 export default function HomePage() {
     return (
-        <div>
-            HomePage
-        </div>
+        <>
+            <Header />
+        </>
     )
 }
